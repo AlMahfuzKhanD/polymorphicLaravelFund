987ade18477e82e48c0a261385efddb56a22be44
mahfuz380 <almahfuz380@gmail.com>
2020-12-14 02:03:09 +0600

    polymorphic done

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,38 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Staff;
+use App\Photo;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/create', function(){
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->create(['path'=>'example.jpg']);
+});
+
+Route::get('/read', function(){
+    $staff = Staff::findOrFail(1);
+
+    foreach($staff->photos as $photo){
+        echo $photo->path;
+    }
+});
+
+Route::get('/update', function(){
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+    $photo->path = "updated.jpg";
+    $photo->save();
+});
+
+Route::get('/delete', function(){
+    $staff = Staff::findOrFail(1);
+    $staff->photos()->whereId(1)->delete();
+});
+
+
