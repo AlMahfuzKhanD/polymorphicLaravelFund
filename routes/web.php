ae8bf8c4736f5956c1adb7b2b16582a662b313df
mahfuz380 <almahfuz380@gmail.com>
2020-12-15 01:03:41 +0600

    polymorphic many to many done

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@
 */
 use App\Staff;
 use App\Photo;
+use App\Post;
+use App\Tag;
+use App\Video;
 
 Route::get('/', function () {
     return view('welcome');
@@ -44,4 +47,58 @@ Route::get('/delete', function(){
     $staff->photos()->whereId(1)->delete();
 });
 
+
+Route::get('/createmor', function(){
+
+    $post = Post::create(['name'=>'First post']);
+
+    $tag1 = Tag::findOrFail(1);
+
+    $post->tags()->save($tag1);
+    
+
+    $video = Video::create(['name'=>'ddd.mov']);
+
+    $tag2 = Tag::findOrFail(2);
+
+    $video->tags()->save($tag2);
+    
+
+
+
+});
+
+Route::get('/readmor', function(){
+    $post = Post::findOrFail(8);
+
+    foreach($post->tags as $tag){
+        echo $tag->name;
+    }
+});
+
+Route::get('/updatemor', function(){
+
+
+    // $post = Post::findOrFail(8);
+    // foreach($post->tags as $tag){
+    //     $tag->whereName('Php')->update(['name'=>'PHP']);
+    // }
+
+    $post = Post::findOrFail(3);
+    $tag = Tag::findOrFail(3);
+
+    // $post->tags()->save( $tag);
+
+    $post->tags()->attach( $tag);
+
+
+});
+
+Route::get('/deletemor', function(){
+    $post = Post::findOrFail(1);
+
+    foreach($post->tags as $tag){
+            $tag->whereId(1)->delete();
+    }
+});
 
